fcc67f38aa1eb0f556510202a946afd19da4ecf7
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-02 14:10:54 +0100

    Added Class ParkedVehicle in Model folder!

---
+++ Garage2/Models/ParkedVehicle.cs
@@ -0,0 +1,16 @@
+﻿namespace Garage2.Models
+{
+    public class ParkedVehicle
+    {
+        public int ParkedVehicleId { get; set; }
+        public string VehicleType { get; set; } = string.Empty;
+        public string RegistrationNumber { get; set;} = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int? Year { get; set; }
+        public string? Color { get; set; }
+        public int? NumberOfWheels { get; set; }
+        public DateTime TimeOfArrival { get; } = DateTime.Today;
+
+    }
+}
