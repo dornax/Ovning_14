4d3583d609247bd5adf40bccfffa33c5057757ba
Horieh Ghiasidalalian <horiehghiasi@gmail.com>
2023-11-03 10:38:42 +0100

    Add class of ParkedVehicleViewModel in Model folder.

---
+++ Garage2/Models/ParkedVehicleViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Garage2.Models
+{
+    public class ParkedVehicleViewModel
+    {
+        string RegNo { get; set; }
+        public DateTime TimeOfArrival { get; } = DateTime.Today;
+        public string Type { get; set; } = string.Empty;
+
+
+
+    }
+}
