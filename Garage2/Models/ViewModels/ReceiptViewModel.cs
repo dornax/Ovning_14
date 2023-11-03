9cec4d7e2bb3bc7785bf656cd21ca3629ec104b5
NinaAlejandra <niinaa___@hotmail.com>
2023-11-03 14:41:00 +0100

    Adding props for price and time of departure and a method for calculating time and price of stay

--- Garage2/Models/ViewModels/ReceiptViewModel.cs
+++ Garage2/Models/ViewModels/ReceiptViewModel.cs
@@ -5,7 +5,20 @@
         public int ParkedVehicleId { get; set; }
         public string VehicleType { get; set; } = string.Empty;
         public string RegistrationNumber { get; set; } = string.Empty;
- 
+
         public DateTime TimeOfArrival { get; set; } = DateTime.Today;
+
+        public DateTime TimeOfDeparture { get; set; } = DateTime.Today;
+
+        public decimal Price { get; set; }
+
+
+        public void CalculateTimeAndPrice()
+        {
+            TimeSpan timeParked = TimeOfDeparture - TimeOfArrival;
+            //if price is 10 kr per hour.
+            Price = (decimal)timeParked.TotalHours * 10;
+        }
+
     }
 }
