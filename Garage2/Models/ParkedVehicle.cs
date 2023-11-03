d6dd9924a33fae50551b313d44bda1128eed7c8f
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-03 12:11:37 +0100

    Adds anotations to ParkedVehiclesModel and restor the set; on TimeOfArrival to try fix a database issue.

--- Garage2/Models/ParkedVehicle.cs
+++ Garage2/Models/ParkedVehicle.cs
@@ -1,16 +1,60 @@
-﻿namespace Garage2.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage2.Models
 {
     public class ParkedVehicle
     {
+        [Key]
+        [DisplayName("ID")]
         public int ParkedVehicleId { get; set; }
+
+
+
+        [DisplayName("Vehicle type")]
+        [Required]
         public string VehicleType { get; set; } = string.Empty;
+
+
+
+        [DisplayName("Registration number")]
+        [Required]
+        [RegularExpression(@"^[A-Za-z]{3}\d{3}$", ErrorMessage = "Registration number must consist of 6 characters, 3 letters followed by 3 digits.")]
+        [StringLength(6, ErrorMessage = "Registration number must consist of 6 characters, 3 letters followed by 3 digits.")]
         public string RegistrationNumber { get; set;} = string.Empty;
+
+
+
+        [StringLength(25, ErrorMessage = "")]
+        [Required]
         public string Make { get; set; } = string.Empty;
+
+
+
+        [StringLength(25, ErrorMessage = "")]
+        [Required]
         public string Model { get; set; } = string.Empty;
+
+
+
+        [Range(1930, 3000, ErrorMessage = "")]
         public int? Year { get; set; }
+
+
+
+        [StringLength(25, ErrorMessage = "")]
         public string? Color { get; set; }
+
+
+
+        [DisplayName("Number of wheels")]
+        [Range(0,16, ErrorMessage = "")]
         public int? NumberOfWheels { get; set; }
-        public DateTime TimeOfArrival { get; } = DateTime.Today;
+
+
+
+        [DisplayName("Time of arrival")]
+        public DateTime TimeOfArrival { get; set; } = DateTime.Today;
 
     }
 }
