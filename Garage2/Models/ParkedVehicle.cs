7436e35cfc0b3b39b393b8b535ce7ca3ac03aefc
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-03 14:34:28 +0100

    Added a new migration. (Tested to do a "update-database" and run the program. Had to do some consequential changes. After that everything works as expected.)

--- Garage2/Models/ParkedVehicle.cs
+++ Garage2/Models/ParkedVehicle.cs
@@ -20,35 +20,35 @@ namespace Garage2.Models
         [DisplayName("Registration number")]
         [Required]
         [RegularExpression(@"^[A-Za-z]{3}\d{3}$", ErrorMessage = "Registration number must consist of 6 characters, 3 letters followed by 3 digits.")]
-        [StringLength(6, ErrorMessage = "Registration number must consist of 6 characters, 3 letters followed by 3 digits.")]
+        [StringLength(6)]
         public string RegistrationNumber { get; set;} = string.Empty;
 
 
 
-        [StringLength(25, ErrorMessage = "")]
+        [StringLength(25)]
         [Required]
         public string Make { get; set; } = string.Empty;
 
 
 
-        [StringLength(25, ErrorMessage = "")]
+        [StringLength(25)]
         [Required]
         public string Model { get; set; } = string.Empty;
 
 
 
-        [Range(1930, 3000, ErrorMessage = "")]
+        [Range(1930, 3000)]
         public int? Year { get; set; }
 
 
 
-        [StringLength(25, ErrorMessage = "")]
+        [StringLength(25)]
         public string? Color { get; set; }
 
 
 
         [DisplayName("Number of wheels")]
-        [Range(0,16, ErrorMessage = "")]
+        [Range(0,16)]
         public int? NumberOfWheels { get; set; }
 
 
