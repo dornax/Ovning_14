fd33284283d388c3fa08f680d3e17f9c02d1f99b
Björn Hansen <dornax@msn.com>
2023-11-08 20:25:04 +0100

    Merge Production to bjorn + layout fix

---
+++ Garage2/Models/ViewModels/OverviewViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Garage2.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage2.Models.ViewModels
+{
+    public class OverviewViewModel
+    {
+
+
+        public int ParkedVehicleId { get; set; }
+        public VehicleTypes VehicleType { get; set; }
+
+        public string RegistrationNumber { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public string? Color { get; set; }
+        public DateTime TimeOfArrival { get; } = DateTime.Today;
+
+
+
+
+
+
+    }
+}
