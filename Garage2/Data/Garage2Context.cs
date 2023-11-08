c98397b4f107ff4010566d3dd87dfc52137aea97
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-08 13:27:09 +0100

    First seed data successful test.

--- Garage2/Data/Garage2Context.cs
+++ Garage2/Data/Garage2Context.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Garage2.Models;
+using Garage2.Models.ViewModels;
+using Garage2.Models.Entities;
 
 namespace Garage2.Data
 {
@@ -15,5 +17,25 @@ namespace Garage2.Data
         }
 
         public DbSet<Garage2.Models.ParkedVehicle> ParkedVehicle { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ParkedVehicle>().HasData(
+                new ParkedVehicle
+                {
+                    ParkedVehicleId = 1,
+                    VehicleType = VehicleTypes.Car,
+                    RegistrationNumber = "ABC123",
+                    Make = "Volvo",
+                    Model = "V70",
+                    Year = 2022,
+                    Color = "White",
+                    NumberOfWheels = 4,
+                    TimeOfArrival = DateTime.Now - TimeSpan.FromHours(1.25)
+                }
+            );
+        }
     }
 }
---
+++ Garage2/Migrations/20231108122439_FirstSeedDataTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Garage2.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstSeedDataTest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ParkedVehicle",
+                columns: new[] { "ParkedVehicleId", "Color", "Make", "Model", "NumberOfWheels", "RegistrationNumber", "TimeOfArrival", "VehicleType", "Year" },
+                values: new object[] { 1, "White", "Volvo", "V70", 4, "ABC123", new DateTime(2023, 11, 8, 12, 9, 39, 99, DateTimeKind.Local).AddTicks(8056), 2, 2022 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ParkedVehicle",
+                keyColumn: "ParkedVehicleId",
+                keyValue: 1);
+        }
+    }
+}
