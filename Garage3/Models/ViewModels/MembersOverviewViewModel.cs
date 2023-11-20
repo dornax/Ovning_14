02fd32a7ace4121c7aac806a7c236558eb089fa3
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-20 10:46:21 +0100

    Skapar två nya ViewModels för Members (Overview och Detailview)!

---
+++ Garage3/Models/ViewModels/MembersOverviewViewModel.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+namespace Garage3.Models.ViewModels
+{
+    public class MembersOverviewViewModel
+    {
+        public int Id { get; set; }
+        public string PersonNo { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
