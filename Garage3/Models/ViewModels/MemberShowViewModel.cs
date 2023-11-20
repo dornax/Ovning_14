42a7cff5956421762b5c4a1ab74d0a909130bc31
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-20 16:35:44 +0100

    Ändrar namnen på och uppdaterar innehållet i ViewModels för Member.

--- Garage3/Models/ViewModels/MemberShowViewModel.cs
+++ Garage3/Models/ViewModels/MemberShowViewModel.cs
@@ -1,12 +1,20 @@
 ﻿#nullable disable
+using Garage3.Controllers;
+
 namespace Garage3.Models.ViewModels
 {
-    public class MemberDetailviewViewModel
+    public class MemberShowViewModel
     {
         public int Id { get; set; }
         public string PersonNo { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public ICollection<Vehicle> Vehicles { get; set; }
+        public int NoOfVehicles { get; set; }
+
+        public void ClaculateNoOfVehicles()
+        {
+            NoOfVehicles = Vehicles.Count;
+        }
     }
 }
---
+++ Garage3/Models/ViewModels/MembersEditNewViewModel.cs
@@ -0,0 +1,27 @@
+﻿#nullable disable
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Garage3.Models.ViewModels
+{
+    public class MembersEditNewViewModel
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [DisplayName("Social security number")]
+        public string PersonNo { get; set; }
+
+
+        [Required]
+        [DisplayName("Fist name")]
+        public string FirstName { get; set; }
+
+
+        [Required]
+        [DisplayName("Last name")]
+        [Compare(nameof(FirstName))]
+        public string LastName { get; set; }
+    }
+}
