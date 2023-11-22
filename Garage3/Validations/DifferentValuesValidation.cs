dcaa067d4ca9dd7aa09fe5c374e0b57db2df92d8
MagFra <47413425+magfra@users.noreply.github.com>
2023-11-22 11:43:15 +0100

    Ree doo

---
+++ Garage3/Validations/DifferentValuesValidation.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage3.Validations
+{
+    public class DifferentValuesValidation : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+
+            const string errorMessage = "Fail from Kalle Anka";
+
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
