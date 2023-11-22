a34becaac24b419216a28b51ef9ed621cefe06fc
NinaAlejandra <niinaa___@hotmail.com>
2023-11-22 13:59:40 +0100

    Adding new VehicleTypesController and moving existing code from VehiclesController t o VehicleTypesController. Adjusting views as well.

--- Garage3/Controllers/VehiclesController.cs
+++ Garage3/Controllers/VehiclesController.cs
@@ -24,7 +24,7 @@ namespace Garage3.Controllers
         // GET: Vehicles
         public async Task<IActionResult> Index()
         {
-            var garage3Context = _context.Vehicles.Include(v => v.Member).Include(v => v.ParkingSpace).Include(v => v.Type);
+            var garage3Context = _context.Vehicles.Include(v => v.Member).Include(v => v.ParkingSpace).Include(v => v.VehicleType);
             return View(await garage3Context.ToListAsync());
         }
 
@@ -39,7 +39,7 @@ namespace Garage3.Controllers
             var vehicle = await _context.Vehicles
                 .Include(v => v.Member)
                 .Include(v => v.ParkingSpace)
-                .Include(v => v.Type)
+                .Include(v => v.VehicleType)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (vehicle == null)
             {
@@ -145,7 +145,7 @@ namespace Garage3.Controllers
             var vehicle = await _context.Vehicles
                 .Include(v => v.Member)
                 .Include(v => v.ParkingSpace)
-                .Include(v => v.Type)
+                .Include(v => v.VehicleType)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (vehicle == null)
             {
@@ -178,164 +178,6 @@ namespace Garage3.Controllers
         {
             return (_context.Vehicles?.Any(e => e.Id == id)).GetValueOrDefault();
         }
-        //Hämtar alla fordonstyper från databasen och skapar en lista av ViewModel-objekt
-        public async Task<IActionResult> ListVehicleTypes()
-        {
-            var vehicleTypes = await _context.VehicleTypes.ToListAsync();
-            var viewModelList = new List<VehicleTypeViewModel>();
-
-            // Loopar igenom varje fordonstyp och skapar ett VehicleTypeViewModel-objekt för varje typ.
-            // Därefter läggs varje skapat objekt till i listan för att användas i vyn.
-            foreach (var vt in vehicleTypes)
-            {
-                var viewModel = new VehicleTypeViewModel
-                {
-                    Id = vt.Id,
-                    Type = vt.Type,
-                    Quantity = GetQuantityForVehicleType(vt.Id)
-                };
-                viewModelList.Add(viewModel);
-            }
-            //Skickar listan av ViewModel-objekt till vyn
-            return View(viewModelList);
-        }
-
-
-        public int GetQuantityForVehicleType(int vehicleTypeId)
-        {
-            //hämtar fordonstypen från databasen inklusive dess fordon
-            var vehicleType = _context.VehicleTypes
-                .Include(vt => vt.Vehicles) // Inkluderar fordonen för att kunna räkna dem
-                .FirstOrDefault(vt => vt.Id == vehicleTypeId);
-
-            if (vehicleType == null) // Om fordonstypen inte finns, returnera 0
-            {
-                return 0;
-            }
-
-            ////Räknar antalet fordon i samlingen för den här fordonstypen
-            return vehicleType.Vehicles.Count;
-        }
-
-        public IActionResult CreateVehicleType()
-        {
-
-            return View();
-
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> CreateVehicleType([Bind("Type")] VehicleType vehicleType)
-        {
-            if (_context.VehicleTypes.Any(vt => vt.Type == vehicleType.Type))
-            {
-                ModelState.AddModelError("Type", "Type already exists.");
-                return View(vehicleType);
-            }
-            if (ModelState.IsValid)
-            {
-                var newVehicleType = new VehicleType { Type = vehicleType.Type };
-
-                _context.Add(newVehicleType);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(ListVehicleTypes));
-            }
-
-            return View(vehicleType);
-        }
-
-
-        public async Task<IActionResult> EditVehicleType(int? id)
-        {
-            if (id == null || _context.VehicleTypes == null)
-            {
-                return NotFound();
-            }
-
-            var vehicleType = await _context.VehicleTypes.FindAsync(id);
-            if (vehicleType == null)
-            {
-                return NotFound();
-            }
-
-            return View(vehicleType);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> EditVehicleType(int id, [Bind("Id,Type")] VehicleType vehicleType)
-        {
-            if (id != vehicleType.Id)
-            {
-                return NotFound();
-            }
-
-            if (_context.VehicleTypes.Any(vt => vt.Id != id && vt.Type == vehicleType.Type))
-            {
-                ModelState.AddModelError("Type", "Type already exists.");
-                return View(vehicleType);
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(vehicleType);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!VehicleExists(vehicleType.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(ListVehicleTypes));
-            }
-          
-            return View(vehicleType);
-        }
-
-        public async Task<IActionResult> DeleteVehicleType(int? id)
-        {
-            if (id == null || _context.VehicleTypes == null)
-            {
-                return NotFound();
-            }
-
-            var vehicleType = await _context.VehicleTypes
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (vehicleType == null)
-            {
-                return NotFound();
-            }
-
-            return View(vehicleType);
-        }
-
-        [HttpPost, ActionName("DeleteVehicleType")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Deleted(int id)
-        {
-            if (_context.VehicleTypes == null)
-            {
-                return Problem("Entity set 'Garage3Context.VehicleTypes'  is null.");
-            }
-            var vehicleType = await _context.VehicleTypes.FindAsync(id);
-            if (vehicleType != null)
-            {
-                _context.VehicleTypes.Remove(vehicleType);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(ListVehicleTypes));
-        }
-
 
     }
 }
